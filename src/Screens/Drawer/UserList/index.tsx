4a139160acf0ca7a94d3983c8908ccd4bc2ac08a
119wjw <119wjw@naver.com>
2020-09-19 21:22:50 +0900

    drawer내부 user목록 구현

---
+++ src/Screens/Drawer/UserList/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+interface Props {
+  user: string;
+}
+
+const UserList = ({user}: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.userText}>{user}</Text>
+    </View>
+  );
+};
+
+export default UserList;
+
+const styles = StyleSheet.create({
+  container: {
+    margin: '1%',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: 'black',
+  },
+  userText: {
+    margin: '4%',
+    fontWeight: '600',
+    color: 'black',
+  },
+});
--- src/Screens/Drawer/index.tsx
+++ src/Screens/Drawer/index.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useContext} from 'react';
 import {
-  ScrollView,
+  View,
+  FlatList,
   SafeAreaView,
   Text,
   StyleSheet,
@@ -12,23 +13,34 @@ import {
   DrawerContentOptions,
 } from '@react-navigation/drawer';
 import {UserContext} from '~/Context/User';
+import UserList from './UserList';
 
 interface Props {
   props: DrawerContentComponentProps<DrawerContentOptions>;
 }
 
+const makeTestUserList = (n: number): string[] => {
+  let users = [];
+  for (let i = 1; i <= n; i++) {
+    users.push('testUser' + i);
+  }
+  return users;
+};
+
 const Drawer = ({props}: Props) => {
   const {logout} = useContext<IUserContext>(UserContext);
-  const [users, setUsers] = useState<string[]>(['wizi']);
+  const [users, setUsers] = useState<string[]>(makeTestUserList(20));
+
+  const renderItem = ({item}: any) => {
+    return <UserList user={item} />;
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.userList}>UserList</Text>
-      <ScrollView style={styles.userListContainer}>
-        <SafeAreaView>
-          <Text>test</Text>
-        </SafeAreaView>
-      </ScrollView>
+      <Text style={styles.userListText}>UserList</Text>
+      <View style={styles.userListContainer}>
+        <FlatList data={users} renderItem={renderItem} />
+      </View>
       <TouchableOpacity style={styles.logoutContainer} onPress={() => logout()}>
         <Image source={require('~/Assets/Images/exit.png')} />
         <Text style={styles.logoutText}> Logout </Text>
@@ -42,22 +54,23 @@ export default Drawer;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'grey',
+    backgroundColor: '#BDBDBD',
   },
-  userList: {
+  userListText: {
+    color: 'white',
     marginTop: '15%',
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: '600',
-    borderBottomWidth: 1,
-    borderBottomColor: 'black',
+    margin: '4%',
   },
   userListContainer: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#D8D8D8',
   },
   logoutContainer: {
     flexDirection: 'row',
     marginBottom: '8%',
+    margin: '4%',
   },
   logoutText: {
     fontSize: 24,
